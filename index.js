5fc2f3ce0caefa6447c85a10098e286b0d4b7a2a
Ilya Lopatin <mrmochin@yandex.ru>
2023-09-11 14:35:33 +0700

    3 tasks have been completed

--- index.js
+++ index.js
@@ -1,5 +1,27 @@
-const fs = require('fs')
+// Task 1
+// const arr = [1, 6, -1, 22, 13];
+// let max = 0;
+// let min = 0;
+// for (let index = 0; index < arr.length; index++) {
+//     if (max < arr[index]) {
+//         max = arr[index]
+//     }
+//     if (min > arr[index]) {
+//         min = arr[index]
+//     }
+// }
+// console.log(max, min)
+//Task 2
+// const str = "!тевирП";
+// console.log(str.split('').reverse().join(''))
+//Task 3
+// const array = [3, 5, 8, 13, 21, 42];
+// let sum = 0;
+// for (let index = 0; index < array.length; index++) {
+//     if (array[index] % 2 == 0) {
+//         sum += array[index] ** 2
+//     }
+// }
+// console.log(sum)
+//Task 4
 
-let res1 = fs.readFileSync('hello.txt', 'utf-8')
-console.log(res1)
-// fs.writeFileSync('hello.txt', 'Hello world!')
